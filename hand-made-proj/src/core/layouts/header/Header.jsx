41491d32f246d3ab581a92ec107674072bd05a18
phuonglinhtht <phuonglinhtht321@gmail.com>
2023-12-21 16:14:17 +0700

    commit FE

--- hand-made-proj/src/core/layouts/header/Header.jsx
+++ hand-made-proj/src/core/layouts/header/Header.jsx
@@ -1,16 +1,14 @@
-import { Avatar, Badge, Button, Dropdown, Tooltip } from 'antd';
-import React, { useMemo } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { NavLink, useNavigate } from 'react-router-dom';
-import BagIcon from '../../../assets/icon/bagIcon.svg';
-import LogoutIcon from '../../../assets/icon/logoutIcon.svg';
-import NoteIcon from '../../../assets/icon/noteIcon.svg';
-import SearchIcon from '../../../assets/icon/searchIcon.svg';
-import AvatarIcon from '../../../assets/img/avatar-2.png';
-import Logo from '../../../assets/img/logo.svg';
-import { actions, selectors } from '../../../stores';
-import './style.scss';
-import { UserOutlined } from '@ant-design/icons';
+import { LoginOutlined, SearchOutlined, UserOutlined } from "@ant-design/icons";
+import { Avatar, Badge, Button, Dropdown, Tooltip } from "antd";
+import React, { useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink, useNavigate } from "react-router-dom";
+import BagIcon from "../../../assets/icon/bagIcon.svg";
+import LogoutIcon from "../../../assets/icon/logoutIcon.svg";
+import NoteIcon from "../../../assets/icon/noteIcon.svg";
+import Logo from "../../../assets/img/logo_cut_trans.png";
+import { actions, selectors } from "../../../stores";
+import "./style.scss";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -25,26 +23,28 @@ const Header = () => {
   const items = useMemo(() => {
     return [
       {
-        key: '1',
+        key: "1",
         label: (
           <div
             onClick={() => {
-              return handleNavigate('/thong-tin-ca-nhan');
-            }}>
+              return handleNavigate("/thong-tin-ca-nhan");
+            }}
+          >
             Thông tin cá nhân
           </div>
         ),
       },
       {
-        key: '2',
+        key: "2",
         label: (
           <div
             onClick={() => {
-              localStorage.removeItem('user');
+              localStorage.removeItem("user");
               dispatch(actions.resetUser());
               dispatch(actions.setCartProducts([]));
-              return handleNavigate('/');
-            }}>
+              return handleNavigate("/");
+            }}
+          >
             Đăng xuất
           </div>
         ),
@@ -55,69 +55,100 @@ const Header = () => {
   return (
     <>
       <header>
-        <NavLink to='/'>
-          <img src={Logo} alt='logo' style={{ width: '210px', height: '150px' }} />
-        </NavLink>
-        <div className='header-container'>
-          <div className='up'>
-            <div className='input'>
-              <input
-                placeholder='Tìm kiếm tại đây ...'
-                onClick={() => navigate('/cua-hang#search')}
-                readOnly
+        <div className="header-container">
+          <div className="down">
+            <NavLink to="/" className="logo">
+              <img
+                src={Logo}
+                alt="logo"
+                style={{ width: "auto", height: "60px" }}
               />
-              <div className='incon-search'>
-                <img src={SearchIcon} alt='logo' />
-              </div>
-            </div>
-            {!userInfo ? (
-              <>
-                <Tooltip title='Đăng nhập/Đăng ký'>
-                  <div className='icon-up' onClick={() => handleNavigate('/dang-nhap')}>
-                    <img src={LogoutIcon} alt='logo' />
-                  </div>
-                </Tooltip>
-              </>
-            ) : (
-              <>
-                <Tooltip title='Giỏ hàng'>
-                  <Badge count={products && products.length ? products.length : null}>
-                    <div className='icon-up' onClick={() => handleNavigate('/gio-hang-cua-toi')}>
-                      <img src={BagIcon} alt='logo' />
-                    </div>
-                  </Badge>
-                </Tooltip>
-                <Tooltip title='Đơn hàng' onClick={() => handleNavigate('/don-hang-cua-toi')}>
-                  <div className='icon-up'>
-                    <img src={NoteIcon} alt='logo' />
-                  </div>
-                </Tooltip>
-                <div className='icon-up'>
-                  <Dropdown
-                    menu={{
-                      items,
-                    }}
-                    placement='bottom'
-                    arrow>
-                    <Avatar src={userInfo.avatar} size='large' icon={<UserOutlined />} />
-                  </Dropdown>
-                </div>
-              </>
-            )}
-          </div>
-          <div className='down'>
-            <Button type='primary' size='large' onClick={() => handleNavigate('/')}>
+            </NavLink>
+            <Button
+              type="primary"
+              size="large"
+              onClick={() => handleNavigate("/")}
+            >
               Trang chủ
             </Button>
-            <Button type='primary' size='large' onClick={() => handleNavigate('/cua-hang')}>
+            <Button
+              type="primary"
+              size="large"
+              onClick={() => handleNavigate("/cua-hang")}
+            >
               Sản phẩm
             </Button>
-            <Button type='primary' size='large' onClick={() => handleNavigate('/ve-chung-toi')}>
+            <Button
+              type="primary"
+              size="large"
+              onClick={() => handleNavigate("/ve-chung-toi")}
+            >
               Giới thiệu
             </Button>
-            <Button type='primary' size='large'>
+            <Button type="primary" size="large">
               Mã giảm giá
             </Button>
+            <Button
+              icon={<SearchOutlined />}
+              size="large"
+              onClick={() => handleNavigate("/cua-hang#search")}
+            >
+              Search
+            </Button>
+
+            <div className="up">
+              {!userInfo ? (
+                <Button
+                  icon={<LoginOutlined />}
+                  type="primary"
+                  size="large"
+                  style={{ backgroundColor: "#16a34a", color: "#ffffff" }}
+                  onClick={() => handleNavigate("/dang-nhap")}
+                >
+                  Đăng nhập
+                </Button>
+              ) : (
+                <>
+                  <Tooltip title="Giỏ hàng">
+                    <Badge
+                      count={
+                        products && products.length ? products.length : null
+                      }
+                    >
+                      <div
+                        className="icon-up"
+                        onClick={() => handleNavigate("/gio-hang-cua-toi")}
+                      >
+                        <img src={BagIcon} alt="logo" />
+                      </div>
+                    </Badge>
+                  </Tooltip>
+                  <Tooltip
+                    title="Đơn hàng"
+                    onClick={() => handleNavigate("/don-hang-cua-toi")}
+                  >
+                    <div className="icon-up">
+                      <img src={NoteIcon} alt="logo" />
+                    </div>
+                  </Tooltip>
+                  <div className="icon-up">
+                    <Dropdown
+                      menu={{
+                        items,
+                      }}
+                      placement="bottom"
+                      arrow
+                    >
+                      <Avatar
+                        src={userInfo.avatar}
+                        size="large"
+                        icon={<UserOutlined />}
+                      />
+                    </Dropdown>
+                  </div>
+                </>
+              )}
+            </div>
           </div>
         </div>
       </header>
