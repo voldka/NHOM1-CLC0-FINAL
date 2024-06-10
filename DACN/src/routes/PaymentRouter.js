62a8739c5a468a83741decf282df1a92c641bed6
Lê Minh Tài <tailx0913@gmail.com>
2024-06-10 12:06:18 +0700

    leminhtai

--- DACN/src/routes/PaymentRouter.js
+++ DACN/src/routes/PaymentRouter.js
@@ -7,7 +7,7 @@ dotenv.config()
 router.get('/config', (req, res) => {
   return res.status(200).json({
     status: 'OK',
-    // data: process.env.CLIENT_ID
+    data: process.env.CLIENT_ID
   })
 })
 
